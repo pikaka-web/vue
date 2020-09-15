405e161b05648bd84526c85f102c734a14d0aace
皮卡卡 <56927876+pikaka-web@users.noreply.github.com>
2020-09-15 19:09:18 +0800

    Add files via upload

---
+++ 8-vue UI框架/test01/a/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HelloWorld></HelloWorld>
+    <Rate></Rate>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Rate from './components/Rate.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Rate,
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ /* margin-top: 60px;*/
+}
+</style>
---
+++ 8-vue UI框架/test01/a/src/components/Rate.vue
@@ -0,0 +1,27 @@
+﻿<template>
+
+    <div class="block">
+        <span class="demonstration">区分颜色</span>
+        <el-rate
+                v-model="value2"
+                :colors="colors">
+        </el-rate>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "Rate",
+		data() {
+			return {
+				value1: null,
+				value2: null,
+				colors: ['#99A9BF', '#F7BA2A', '#FF9900']  // 等同于 { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
